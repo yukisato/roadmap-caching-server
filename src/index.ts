f3b09c62e228aa91616fc15947a4c6c9405946dc
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-27 20:38:15 -0700

    feat(run): create initial code base

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { run } from './run';
+
+(async () => {
+  try {
+    await run(process.argv);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+  }
+})();
