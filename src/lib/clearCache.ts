3060d551e3a164f601187e275a3443dd413d787a
Yuki Sato <yuki.sato.dev@gmail.com>
2024-08-27 23:34:30 -0700

    refactor(run): move operationa logic to the first in run()

---
+++ src/lib/clearCache.ts
@@ -0,0 +1,3 @@
+export const clearCache = () => {
+  console.log('TODO: Clearing cache');
+};
--- src/run.ts
+++ src/run.ts
@@ -1,23 +1,22 @@
+import { clearCache } from '@/lib/clearCache';
 import {
   getOperationalOptions,
   getServerOptions,
 } from './lib/commandOptionPrser';
 
 export const run = async (argv: string[]) => {
+  // Operate the proxy server if the command line arguments are for the operations
+  const operationalOptions = getOperationalOptions(argv);
+  if (operationalOptions?.clearCache) {
+    clearCache();
+    return;
+  }
+
   // Start the proxy server if the command line arguments are for the server
   const serverOptions = getServerOptions(argv);
   if (serverOptions) {
     listen(serverOptions.port, serverOptions.origin);
     return;
-  }
-
-  // Operate the proxy server if the command line arguments are for the operations
-  const operationalOptions = getOperationalOptions(argv);
-  if (operationalOptions?.clearCache) {
-    console.log('TODO: Clearing cache');
-    return;
-  } else {
-    console.error('Missing command line arguments');
   }
 };
 
